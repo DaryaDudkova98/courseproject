47ddb4d4d6128a080546b2d341371914d1412544
DaryaDudkova98 <dudkovadaryadmitrievna@mail.ru>
2025-12-21 21:29:51 +0300

    Update

--- src/Security/AuthenticationEntryPoint.php
+++ src/Security/AuthenticationEntryPoint.php
@@ -6,19 +6,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 
 class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
 {
     public function __construct(
         private UrlGeneratorInterface $urlGenerator,
-        private FlashBagInterface $flashBag,
     ) {}
 
     public function start(Request $request, ?AuthenticationException $authException = null): RedirectResponse
     {
-        $this->flashBag->add('note', 'You have to login in order to access this page.');
-
+        // Без флеша, только редирект
         return new RedirectResponse($this->urlGenerator->generate('security_login'));
     }
 }
