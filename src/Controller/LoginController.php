e8899cb113c58cb071e2971e8e5eeb01e2c05a13
DaryaDudkova98 <dudkovadaryadmitrievna@mail.ru>
2025-12-26 15:38:40 +0300

    update oauth

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -28,5 +29,11 @@ class LoginController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/login/check-google', name: 'login_check_google')]
+    public function loginCheck(Request $request): Response
+    {
+        return $this->redirectToRoute('homepage');
     }
 }
